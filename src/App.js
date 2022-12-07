ba5276707103da414c2bd4434ab3b07903778965
Jun <79557712+JUNUbermensch@users.noreply.github.com>
2022-12-07 19:54:09 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -9,28 +9,28 @@ import TopElement from './components/TopElement';
 import WarningOnText from './components/WarningOnText';
 import InputBox from './components/InputBox';
 import List from './components/List';
+import StopButton from './components/StopButton';
+import WarningPlayer from './components/WarningPlayer';
 
 // 설정법
 // node.js 설치, npm 설치, yarn 설치
 // ((npm create-react-app --use-npm) ==> git 에서 App.js, App.css, components 폴더만 copy)
 // or 모두 copy (project-ui 폴더)
 
-// 메인 화면
-const App = () => {
+// 이후 크롬 설정 --> 개인 정보 보호 및 보안 --> 사이트 설정 --> 추가 콘텐츠 설정
+// --> 소리 --> 기본 동작: 사이트에서 소리를 재생할 수 있음 체크
+// --> 하단에 소리 재생이 허용됨 추가 --> http://localhost:3000 추가
 
-  /*  서버에서 받아올 데이터:
-      상단바: TOD 연결 상태, AI Version 리스트, 경고기능 작동상태
-      중단: (이미지 바이너리), (상황목록)
-      하단바: 화면 정보 (fps, 해상도), 객체 검출 정보 (객체이름, 수량), 상황 (정상, 이상, 경고)
-  */
+// 메인 화면 (index.js에서 index.html로 App.js를 띄워줌)
+const App = () => {
 
   // 각 구성요소 상태 관리 (새로고침 없이 자동 re-rendering을 위해 state로 저장)
   // 서버에서 받을 데이터
   const [AIVersionList, setAIVersionList] = useState(() => ["Loading..."]);
   const [warningOn, setWarningOn] = useState(() => 0);
-  const [myImage, setMyImage] = useState(() => "public/logo192.png");
+  const [myImage, setMyImage] = useState(() => "");
   const [fpsInfo, setFpsInfo] = useState(() => 0);
-  const [scaleInfo, setScaleInfo] = useState(() => "none");
+  const [scaleInfo, setScaleInfo] = useState(() => "320 X 192");
   const [objectHuman, setObjectHuman] = useState(() => 0);
   const [objectAnimal, setObjectAnimal] = useState(() => 0);
   const [objectVehicle, setObjectVehicle] = useState(() => 0);
@@ -39,29 +39,15 @@ const App = () => {
 
 
   // 서버로 보낼 데이터
-  const [AIVersion, setAIVersion] = useState(() => "Select AI ver.");
+  const [AIVersion, setAIVersion] = useState(() => "Select ver.");
   const [warningWaitTime, setWarningWaitTime] = useState(() => 3);
   const [warningOnTime, setWarningOnTime] = useState(() => 3);
 
   // Front 자체 가공 데이터
   const [todConnection, setTodConnection] = useState(() => -1);
   const [logMessages, setLogMessages] = useState([]);
+  const [soundOn, setSoundOn] = useState(() => true);
 
-  // 경고발생 대기시간도 state 관리해서 입력할 수 있게 하기 (TODConnect 참고)
-  // 상황 목록 구현하기 (스크롤바)
-  // 하단부 구현하기
-  
-  /* TODO
-    >> 경고 시간 설정할 때 react-hook-form 사용해서 validation 체크하기
-
-    >> 상황 목록 로그 구현하기 (채팅 앱 구현하는 것처럼 (예전 프로젝트 한 것 참고))
-    >> video 재생 (바이너리 이미지 스트리밍) 구현하기 */
-
-  // 서버와 연결 (데이터 전송할 때 socket을 인자로 넘겨주면 될듯?)
-  // 웹앱 실행 시 일단 바로 웹소켓 연결 (이 동안에는 AI Version을 Loading... 으로 띄우기)
-  // 연결되면 AI Version을 받고 선택해서 연결 번튼을 누르면 해당 AI Version을 JSON으로 보내고 연결 중... 상태로 만들기
-  // 그 후에 서버로부터 image가 전송되면 연결 완료 상태로 만들기.
-  
   // 서버로 데이터 전송하기 위한 상태 관리
   const [sendConnection, setSendConnection] = useState(() => false);
   const [sendWaitTime, setSendWaitTime] = useState(() => false);
@@ -139,7 +125,6 @@ const App = () => {
             setWarningOn(result.warningOn);
             setMyImage("data:image/jpg;base64,"+result.data);
             setFpsInfo(result.fpsInfo);
-            setScaleInfo(result.scaleInfo);
             setObjectHuman(result.objectHuman);
             setObjectAnimal(result.objectAnimal);
             setObjectVehicle(result.objectVehicle);
@@ -165,6 +150,7 @@ const App = () => {
 
   return (
     <div className="App">
+      <WarningPlayer warningOn = { warningOn } soundOn= { soundOn } setSoundOn={ setSoundOn }></WarningPlayer>
 
       <div className="top-area">
         <TopElement>TOD 연결 상태</TopElement>
@@ -173,19 +159,16 @@ const App = () => {
         <TopElement>AI Version</TopElement>
         <Dropdown AIVersion={ AIVersion } setAIVersion={ setAIVersion } AIVersionList={ AIVersionList } />
 
-        <WarningOnText warningOn={ warningOn } />
-
         <TopElement>경고발생 대기시간 (초)</TopElement>
         <InputBox time={ warningWaitTime } setTime={ setWarningWaitTime } setSendTime={ setSendWaitTime } />
 
-        <TopElement>경고기능 켜짐시간 (초)</TopElement>
-        <InputBox time={ warningOnTime } setTime={ setWarningOnTime } setSendTime={ setSendOnTime } />
+        <WarningOnText warningOn={ warningOn } soundOn={ soundOn } />
+        <StopButton warningOn={ warningOn } soundOn={ soundOn } setSoundOn={ setSoundOn } />
 
       </div>
 
       
       <div className="mid-area">
-
         <main className="mid-video">
           <img src={`${myImage}`} alt="TOD Video"/>
         </main>
@@ -193,7 +176,6 @@ const App = () => {
         <aside className="mid-log">
           <List messages={logMessages} setMessages={setLogMessages} warningState={warningState} isChangeState={isChangeState} setIsChangeState={setIsChangeState}></List>
         </aside>
-        
       </div>
 
 
@@ -203,9 +185,9 @@ const App = () => {
         <BottomState warningState={ warningState } />
       </div>
 
-
     </div>
   );
+
 }
 
 export default App;
